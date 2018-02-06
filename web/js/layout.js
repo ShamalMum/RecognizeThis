ba5bc191c517269c4476afe330b48ed376092125
shamalMUM <shamalf1@gmail.com>
2018-02-06 11:18:28 -0600

    code was moved to new layout.js

---
+++ web/js/layout.js
@@ -0,0 +1,20 @@
+$(function () {
+        //page is loaded this is first page request
+        $("#box").load("detecttext");
+
+        $("a").click(function () {
+            if (this.id == 'anchrtext') {
+                $("#box").load("detecttext");
+            }
+            else if (this.id == 'anchobject') {
+                $("#box").load("detectobjects");
+            }
+            else if (this.id == 'anchface') {
+                $("#box").load("detectfaces");
+            }
+            else if (this.id == 'anchcele') {
+                $("#box").load("detectcelebs");
+            }
+        });
+
+    });
